e7193c5ee6d58fe78719e552adde2d64516dbb41
marutanm <marutanm@gmail.com>
2012-01-18 21:29:48 +0900

    just puts API response

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,22 +1,7 @@
 AsyncPoker.controllers  do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
 
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
+  get :index do
+    RestClient.get "http://www.pivotaltracker.com/services/v3/projects/#{ENV['PROJECT_ID']}/stories", {'X-TrackerToken' => ENV['TOKEN'] }
+  end
 
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
-
-  
 end
