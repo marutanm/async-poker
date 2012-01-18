113cca34a04c001d409da84b78f2d711ace915a4
marutanm <marutanm@gmail.com>
2012-01-18 22:59:19 +0900

    get stories from pivotal tracker and save it to db

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,7 +1,6 @@
 AsyncPoker.controllers  do
 
   get :index do
-    RestClient.get "http://www.pivotaltracker.com/services/v3/projects/#{ENV['PROJECT_ID']}/stories", {'X-TrackerToken' => ENV['TOKEN'] }
   end
 
 end
--- app/helpers.rb
+++ app/helpers.rb
@@ -1,7 +1,14 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 AsyncPoker.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+
+  def get_and_save_stories
+    noko = Nokogiri::XML(RestClient.get "http://www.pivotaltracker.com/services/v3/projects/#{ENV['PROJECT_ID']}/stories", {'X-TrackerToken' => ENV['TOKEN'] })
+
+    noko.xpath("//story").each do |n|
+      Story.create(id: n.xpath("id").text, name: n.xpath("name").text)
+    end
+
+  end
+
 end
