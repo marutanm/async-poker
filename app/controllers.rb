e2c9e5cfe91e457b04749b00b6201115c988f91f
marutanm <marutanm@gmail.com>
2012-01-20 08:20:10 +0900

    GET stories

--- app/controllers.rb
+++ app/controllers.rb
@@ -3,4 +3,8 @@ AsyncPoker.controllers  do
   get :index do
   end
 
+  get :update do
+    Pivotal.find(:all, :params => {:project_id => ENV['PROJECT_ID'], :filter => 'story_type:feature'}).to_s
+  end
+
 end
--- models/pivotal.rb
+++ models/pivotal.rb
@@ -3,8 +3,9 @@ class Pivotal < ActiveResource::Base
 
   headers['X-TrackerToken'] = ENV['TOKEN']
   
-  self.site = "http://www.pivotaltracker.com/services/v3"
-  self.element_name = "projects"
+  self.site = "http://www.pivotaltracker.com/"
+  self.prefix = "/services/v3/projects/:project_id/"
+  self.collection_name = "stories"
   self.format = :xml
   
   self.logger = Logger.new($stderr)
