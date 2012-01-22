ea6d6ec6c065cfad098f9adead07140e3d0b1ddc
marutanm <marutanm@gmail.com>
2012-01-22 22:50:40 +0900

    authorize with github

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,5 +1,16 @@
 AsyncPoker.controllers  do
 
+  get '/auth/github/callback' do
+    auth = request.env["omniauth.auth"]
+    if authorized?(auth)
+      user = User.find_or_create_by_ominiauth(auth)
+      set_current_user(user)
+      redirect url(:index)
+    else
+      halt 401
+    end
+  end
+
   get :index do
     @stories = Story.all.each
     haml :index
--- models/user.rb
+++ models/user.rb
@@ -1,9 +1,19 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps # adds created_at and updated_at fields
-  field :name
+  field :nickname
   field :uid
 
   has_many :votes
 
+  def self.find_or_create_by_ominiauth(auth)
+    user = find_or_create_by(uid: auth['uid']) do |u|
+      u.nickname = auth['info']['nickname']
+    end
+  end
+
+  def self.find_by_uid(uid)
+    where(uid: uid).first rescue nil
+  end
+  
 end
