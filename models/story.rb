291bc5c25dbaf30998581f0e4df23c174dda6f6e
marutanm <marutanm@gmail.com>
2012-01-24 23:00:25 +0900

    def some method to story.rb

--- models/story.rb
+++ models/story.rb
@@ -4,9 +4,21 @@ class Story
   field :story_id
   field :name
   field :description
-  field :estimate
+  field :estimate, type: Integer
   field :state, type: Boolean, default: false
 
   has_many :votes
+
+  def self.to_vote
+    where(estimate: -1, state: false)
+  end
+
+  def self.voted
+    where(estimate: -1, state: true)
+  end
+
+  def self.estimated
+    where("this.estimate != -1")
+  end
 
 end
