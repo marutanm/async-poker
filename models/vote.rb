5adee668ec78f7a9258f412dbc0ec1edbfcc0a2c
marutanm <marutanm@gmail.com>
2012-01-24 23:46:58 +0900

    add method to create new vote

--- models/vote.rb
+++ models/vote.rb
@@ -3,8 +3,14 @@ class Vote
   include Mongoid::Timestamps # adds created_at and updated_at fields
 
   belongs_to :story
-  belongs_to :user
+  belongs_to :account
 
-  field :value
+  field :value, type: Integer
+
+  def self.update_value_with(params)
+    find_or_create_by(story_id: params[:story_id], account_id: params[:account_id]) do |v|
+      v.value = params[:value]
+    end
+  end
 
 end
