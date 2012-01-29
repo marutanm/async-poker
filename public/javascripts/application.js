0d573fd5e3cfe339564f5c16d688beba3e2755ca
marutanm <marutanm@gmail.com>
2012-01-29 12:17:20 +0900

    ignore processed javascripts and stylesheets

--- public/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Sat, 28 Jan 2012 13:29:32 GMT from
- * /Users/marutanm/dev/async-poker/assets/javascripts/application.coffee
- */
-
-(function() {
-
-  console.log('barista');
-
-}).call(this);
